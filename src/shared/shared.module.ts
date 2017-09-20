2c466c2f6818223827dee7e74b3c18e3d7fd5c1e
William Lohan <w.lohan@tabsautobiz.com>
2017-09-20 13:34:40 -0700

    init

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * https://angular.io/guide/ngmodule#shared-modules
+ */
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  providers: []
+})
+export class SharedModule { }
