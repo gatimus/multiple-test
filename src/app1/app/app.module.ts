93d0e0fba1d1a4546563f98de088c1ccdb114620
William Lohan <w.lohan@tabsautobiz.com>
2017-09-20 13:53:27 -0700

    use path

--- src/app1/app/app.module.ts
+++ src/app1/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from 'shared/shared.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
